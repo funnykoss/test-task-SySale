4b972e0b8f1e4ba8c2a5c95ba8b35a0eb365a309
funnykoss <75586141+funnykoss@users.noreply.github.com>
2022-01-29 12:16:10 +0200

    add cardView

---
+++ src/view/CardView/CardView.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "../../component/Button";
+
+const CardView = () => {
+    return (
+        <>
+        <Button width="300px" name="купить" />
+        <Button width="71px" name="new" fontSize="14px" fontWeight="500"/>
+        </>
+    )
+}
+
+
+export default CardView
