173b6de1fa050b23c17f254c740e51da67b1fe33
funnykoss <75586141+funnykoss@users.noreply.github.com>
2022-01-29 12:15:28 +0200

    add component button and css

---
+++ src/component/Button/Button.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import s from './Button.module.css';
+
+function Button({
+  name,
+    width = '328px',
+    type = 'button',
+    fontSize = '18px',
+  fontWeight='800'
+ 
+}) {
+  return (
+    <div>
+      <button
+        type={type}
+        className={s.button}
+        style={{  width: `${width}`, fontSize:`${fontSize}`, fontWeight:`${fontWeight}`}}
+      >
+        {name}
+      </button>
+    </div>
+  );
+}
+
+export default Button;
